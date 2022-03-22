7bb82ba972cc8d4579d10d7ee71557f7d968f4f9
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-21 19:55:21 -0500

    Update index

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VMcCurrey Midterm INF653</title>
-</head>
-<body>
-    <h1></h1>
-</body>
-</html>
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    }
+    include_once '../../config/Database.php';
+    include_once '../../models/Quote.php';
+    
+    if ($method == 'GET') {
+        include 'read_single.php';
+    } elseif ($method == 'POST') {
+        include 'create.php';
+    } elseif ($method == 'PUT') {
+        include 'update.php';
+    } elseif ($method == 'DELETE') {
+        include 'delete.php';
+    } 
