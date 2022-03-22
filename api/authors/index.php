0c493359facd61f42cc673dd7bc75bc1ab1ed16c
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-22 18:39:11 -0500

    Update index.php

--- api/authors/index.php
+++ api/authors/index.php
@@ -6,19 +6,17 @@
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     }
-    include_once '../../config/Database.php';
-    include_once '../../models/Author.php';
     
-    if ($method == 'GET') {
-        if(isset($_GET['id']) == false){
-            include 'read.php';
-        } else {
-            include 'read_single.php';
-        }
+    $filterId = filter_input(INPUT_GET, "id");
+    
+    if (!empty($filterId) && $method == 'GET') {
+        include 'read_single.php';
     } elseif ($method == 'POST') {
         include 'create.php';
     } elseif ($method == 'PUT') {
         include 'update.php';
     } elseif ($method == 'DELETE') {
         include 'delete.php';
-    } 
+    } elseif ($method == 'GET') {
+        include 'read.php';
+    }
