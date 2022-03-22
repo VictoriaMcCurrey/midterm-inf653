1170cc70614f6a0257cc65f709deea1fc278e89f
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-21 19:46:04 -0500

    Update create

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,12 +1,4 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    }
-    
     include_once '../../config/Database.php';
     include_once '../../models/Author.php';
     
@@ -20,7 +12,6 @@
     // Get Raw Data
     $data = json_decode(file_get_contents("php://input"));
 
-    $author -> id = $data -> id;
     $author -> author = $data -> author;
 
     // Create Author
