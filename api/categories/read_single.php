8dfcddbd6322a85ec162bba7949fc45e0915fc97
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-21 19:53:25 -0500

    Update read_single

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -1,11 +1,4 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    }
     include_once '../../config/Database.php';
     include_once '../../models/category.php';
 
@@ -17,7 +10,7 @@
     $category = new Category($db);
 
     // GET ID
-    $category -> id = isset($_GET['id']) ? $_GET['id'] : die();
+    $category -> id = isset($_GET['id']) ? $_GET['id'] : die(include 'read.php');
 
     // Get category 
     $category -> read_single();
