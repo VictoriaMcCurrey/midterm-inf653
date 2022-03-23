35b3dd488eadd1bba5d38f78a5e6552674cd7136
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-22 21:04:04 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -14,14 +14,18 @@
 
     $author -> author = $data -> author;
 
+    
+
     // Create Author
     if($author -> create()) {
         echo json_encode(
-            array('Message' => 'Author Created')
-        );
+            array('id' => $db->lastInsertId(),
+                  'author' => $author -> author
+            ));
     } else {
         echo json_encode(
-            array('Message' => 'Author Not Created')
+            array('Message' => 'Missing Required Parameters')
         );
-        exit();
     }
+        exit();
+    
