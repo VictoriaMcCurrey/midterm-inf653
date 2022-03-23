d9e7fe9b3c1527fa2b4f9c21fcb5c7aa72655459
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-22 22:54:19 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -16,7 +16,7 @@
 
     if ($author -> author == null) {
         echo json_encode(
-            array('Message' => 'Missing Required Parameters'));
+            array('message' => 'Missing Required Parameters'));
             exit();
     }
     
@@ -29,7 +29,7 @@
             ));
     } else {
         echo json_encode(
-            array('Message' => 'Missing Required Parameters')
+            array('message' => 'Missing Required Parameters')
         );
     }
         exit();
