39e3232cf5995c373e41836822ae2bf73bbcc186
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-22 19:05:26 -0500

    Update read_single.php

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -1,6 +1,6 @@
 <?php
     include_once '../../config/Database.php';
-    include_once '../../models/category.php';
+    include_once '../../models/Category.php';
 
     // Instantiate DB & Connect
     $database = new Database();
