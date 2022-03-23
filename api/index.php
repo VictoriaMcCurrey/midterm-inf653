8d5f4bdc0f364efe792f27f07e4b37a8ca6f4221
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-23 07:40:48 -0500

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Victoria McCurrey's Midterm INF 653</title>
+</head>
+<body>
+    <h1>Victoria McCurrey's Midterm-API</h1>
+</body>
+</html>
