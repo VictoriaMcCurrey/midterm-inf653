064fcd70f6065e8fee04fc01868a5dafee422807
VictoriaMcCurrey <89602743+VictoriaMcCurrey@users.noreply.github.com>
2022-03-23 08:01:46 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -13,18 +13,21 @@
     $data = json_decode(file_get_contents("php://input"));
 
     // Set ID to DELETE
-    $category -> id = $data -> id;
+  $category->id = $data->id;
 
-    // DELETE category
-    $category -> delete();
-
-    if($category -> id !== null) {
-        echo json_encode(
-            array('id' => $category -> id)
-        );
-    } else {
-        echo json_encode(
-            array('message' => 'category Not Deleted')
-        );
-    }
+  // Delete post
+  $response = $category->delete();
+  if($response > 0) {
+    echo json_encode(
+      array('id' => $category->id)
+    );
+  } else if($response == -1){
+    echo json_encode(
+      array('message' => 'Cant delete: foreign key in use')
+    );
+  }else {
+    echo json_encode(
+      array('message' => 'categoryId Not Found')
+    );
+  }
     exit();
